09f3aacda7c16d13c9be1abc073222ca3899e4c1
soywiz <soywiz@gmail.com>
2018-08-14 08:13:59 +0200

    - HyperLogLog commands

---
+++ src/io/ktor/experimental/client/redis/RedisCommandsHyperLogLog.kt
@@ -0,0 +1,28 @@
+package io.ktor.experimental.client.redis
+
+/**
+ * Adds the specified elements to the specified HyperLogLog.
+ *
+ * https://redis.io/commands/pfadd
+ *
+ * @since 2.8.9
+ */
+suspend fun Redis.pfadd(key: String, vararg elements: String): Long = commandLong("pfadd", key, *elements)
+
+/**
+ * Return the approximated cardinality of the set(s) observed by the HyperLogLog at key(s).
+ *
+ * https://redis.io/commands/pfcount
+ *
+ * @since 2.8.9
+ */
+suspend fun Redis.pfcount(vararg keys: String): Long = commandLong("pfcount", *keys)
+
+/**
+ * Merge N different HyperLogLogs into a single one.
+ *
+ * https://redis.io/commands/pfmerge
+ *
+ * @since 2.8.9
+ */
+suspend fun Redis.pfmerge(destKey: String, vararg sourceKeys: String): Long = commandLong("pfmerge", destKey, *sourceKeys)
