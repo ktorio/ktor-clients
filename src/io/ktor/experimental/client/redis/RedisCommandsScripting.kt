a7b75e5442901c0f4f51080b3cebac9ba099d572
soywiz <soywiz@gmail.com>
2018-08-14 14:58:35 +0200

    - Scripting comments

--- src/io/ktor/experimental/client/redis/RedisCommandsScripting.kt
+++ src/io/ktor/experimental/client/redis/RedisCommandsScripting.kt
@@ -1,14 +1,35 @@
 package io.ktor.experimental.client.redis
 
+/**
+ * Execute a Lua script server side.
+ *
+ * https://redis.io/commands/eval
+ *
+ * @since 2.6.0
+ */
 suspend fun Redis.eval(script: String, vararg args: Pair<String, Any?>): Any? = executeText(
     "eval", script, args.size,
     *(args.map { it.first }.toTypedArray()),
     *(args.map { it.second }.toTypedArray())
 )
 
+/**
+ * Execute a Lua script server side.
+ *
+ * https://redis.io/commands/eval
+ *
+ * @since 2.6.0
+ */
 suspend fun Redis.eval(script: String, args: Map<String, Any?>): Any? =
     eval(script, *(args.map { it.key to it.value }.toTypedArray()))
 
+/**
+ * Execute a Lua script server side.
+ *
+ * https://redis.io/commands/evalsha
+ *
+ * @since 2.6.0
+ */
 suspend fun Redis.evalsha(sha1: String, vararg args: Pair<String, Any?>): Any? {
     return executeText(
         "evalsha", sha1, args.size,
@@ -17,17 +38,60 @@ suspend fun Redis.evalsha(sha1: String, vararg args: Pair<String, Any?>): Any? {
     )
 }
 
+/**
+ * Execute a Lua script server side.
+ *
+ * https://redis.io/commands/evalsha
+ *
+ * @since 2.6.0
+ */
 suspend fun Redis.evalsha(sha1: String, args: Map<String, Any?>): Any? =
     evalsha(sha1, *(args.map { it.key to it.value }.toTypedArray()))
 
+/**
+ * Load the specified Lua script into the script cache.
+ *
+ * https://redis.io/commands/script-load
+ *
+ * @since 2.6.0
+ */
 suspend fun Redis.scriptLoad(script: String): String = this.commandString("SCRIPT", "LOAD", script) ?: ""
 
+/**
+ * Kill the script currently in execution.
+ *
+ * https://redis.io/commands/script-kill
+ *
+ * @since 2.6.0
+ */
 suspend fun Redis.scriptKill(): String = this.commandString("SCRIPT", "KILL") ?: ""
 
+/**
+ * Remove all the scripts from the script cache.
+ *
+ * https://redis.io/commands/script-flush
+ *
+ * @since 2.6.0
+ */
 suspend fun Redis.scriptFlush(): String = this.commandString("SCRIPT", "FLUSH") ?: ""
 
 enum class RedisScriptDebug { Yes, Sync, No }
+
+/**
+ * Set the debug mode for executed scripts.
+ *
+ * https://redis.io/commands/script-debug
+ *
+ * @since 3.2.0
+ */
 suspend fun Redis.scriptDebug(kind: RedisScriptDebug): String = this.commandString("SCRIPT", "DEBUG", kind.name.toUpperCase()) ?: ""
 
+/**
+ * Check existence of scripts in the script cache.
+ *
+ * https://redis.io/commands/script-exists
+ *
+ * @since 2.6.0
+ */
 suspend fun Redis.scriptExists(vararg sha1: String): List<Boolean> =
     this.commandArrayLong("SCRIPT", "EXISTS", *sha1).map { it != 0L }
